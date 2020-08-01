786cb5b66436afa464cd2663b15e7af0fb6e0f5f
leesh1202 <l.sahu@somaiya.edu>
2020-08-01 15:59:53 +0530

    finally corrected

--- lib/service/world_time.dart
+++ lib/service/world_time.dart
@@ -18,15 +18,20 @@ class WorldTime {
 
       //get properties from data
       String datetime = data['datetime'];
-      String offset = data['utc_offset'].substring(1, 3);
-
+      print(datetime);
+      //String offset = data['utc_offset'].substring(0, 3);
+      //print(offset);
       //creta date time object
       DateTime now = DateTime.parse(datetime);
-      now = now.add(Duration(hours: int.parse(offset)));
+      print(now);
+      now = now.add(Duration(
+          hours: int.parse(data['utc_offset'].substring(0, 3)),
+          minutes: int.parse(data['utc_offset'].substring(4, 6))));
       //Set time property
       //time = now.toString();
       isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
       time = DateFormat.jm().format(now);
+      // print(time);
     } catch (e) {
       print('caught error::$e');
       time = 'could not get time data';
